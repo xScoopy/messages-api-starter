e5196d0232d2adb6d94dc9a534c68c1909901fba
xScoopy <JLeary070@gmail.com>
2021-04-13 18:31:53 -0500

    altered routes

--- src/models/user.js
+++ src/models/user.js
@@ -2,3 +2,13 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 // Add your models here.
+const UserSchema = new Schema({
+    username: { type: String, required: true },
+    password: { type: String, select: false }
+  })
+  
+  const User = mongoose.model('User', UserSchema)
+  
+  module.exports = User
+
+  
--- src/routes/user.js
+++ src/routes/user.js
@@ -5,33 +5,55 @@ const User = require('../models/user')
 
 /** Route to get all users. */
 router.get('/', (req, res) => {
-    return res.send(`All Users route`)
+    User.find().then((users) => {
+        return res.json({users})
+    })
+    .catch((err) => {
+        throw err.message
+    });
 })
 
 /** Route to get one user by id. */
 router.get('/:userId', (req, res) => {
-    return res.send(`User with id ${req.params.userId}`)
+    console.log(`User id: ${req.params.userId}`)
+    User.findById(req.params.userId).then((user) => {
+        return res.json({user})
+    })
+    .catch((err) => {
+        throw err.message
+    });
 })
 
 /** Route to add a new user to the database. */
 router.post('/', (req, res) => {
-    return res.send({
-        message: 'Create new user',
-        data: req.body
+    let user = new User(req.body)
+    user.save().then(userResult => {
+        return res.json({user: userResult})
+    }).catch((err) => {
+        throw err.message
     })
 })
 
 /** Route to update an existing user. */
 router.put('/:userId', (req, res) => {
-    return res.send({
-        message: `Update user with id ${req.params.userId}`,
-        data: req.body
+    User.findByIdAndUpdate(req.params.userId, req.body).then((user) => {
+        return res.json({user})
+    }).catch((err) => {
+        throw err.message
     })
 })
 
 /** Route to delete a user. */
 router.delete('/:userId', (req, res) => {
-    return res.send(`Delete user with id ${req.params.userId}`)
+    User.findByIdAndDelete(req.params.userId).then(() => {
+        return res.json({
+            'message': 'Successfully deleted.',
+            '_id': req.params.userId
+        })
+    })
+    .catch((err) => {
+        throw err.message
+    })
 })
 
 module.exports = router
